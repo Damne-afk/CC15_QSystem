6ee1588e8950bb375683abd95904ff5f9831cfee
Damne-afk <83099547+Damne-afk@users.noreply.github.com>
2021-04-28 20:25:24 +0800

    Create main_Admin(updated).py

---
+++ cc15_qsystem_main/main_Admin(updated).py
@@ -0,0 +1,373 @@
+################################################################################
+##
+## BY: WANDERSON M.PIMENTA
+## PROJECT MADE WITH: Qt Designer and PySide2
+## V: 1.0.0
+##
+## This project can be used freely for all uses, as long as they maintain the
+## respective credits only in the Python scripts, any information in the visual
+## interface (GUI) can be modified without any implication.
+##
+## There are limitations on Qt licenses if you want to use your products
+## commercially, I recommend reading them on the official website:
+## https://doc.qt.io/qtforpython/licenses.html
+##
+################################################################################
+
+import sys
+import platform
+from PyQt5 import QtCore, QtGui, QtWidgets
+from PyQt5.QtCore import (QCoreApplication, QPropertyAnimation, QDate, QDateTime, QMetaObject, QObject, QPoint, QRect, QSize, QTime, QUrl, Qt, QEvent)
+from PyQt5.QtGui import (QBrush, QColor, QConicalGradient, QCursor, QFont, QFontDatabase, QIcon, QKeySequence, QLinearGradient, QPalette, QPainter, QPixmap, QRadialGradient)
+from PyQt5.QtWidgets import *
+
+
+from adminAppModules import *
+
+class MainWindow(QMainWindow):
+    def __init__(self):
+        QMainWindow.__init__(self)
+        self.ui = Ui_MainWindow()
+        self.ui.setupUi(self)
+        self.popMenuInits()
+        ## PRINT ==> SYSTEM
+        print('System: ' + platform.system())
+        print('Version: ' + platform.release())
+
+        ########################################################################
+        ## START - WINDOW ATTRIBUTES
+        ########################################################################
+
+        ## REMOVE ==> STANDARD TITLE BAR
+        UIFunctions.removeTitleBar(True)
+        ## ==> END ##
+
+        ## SET ==> WINDOW TITLE
+        self.setWindowTitle('Main Window - Python Base')
+        UIFunctions.labelTitle(self, 'Admin Main Window - Python Base')
+        UIFunctions.labelDescription(self, 'Set text')
+        ## ==> END ##
+
+        ## WINDOW SIZE ==> DEFAULT SIZE
+        startSize = QSize(1440, 810)
+        self.resize(startSize)
+        self.setMinimumSize(1080, 608)
+        #UIFunctions.enableMaximumSize(self, 500, 720)
+        ## ==> END ##
+
+        ## ==> CREATE MENUS
+        ########################################################################
+
+        ## ==> TOGGLE MENU SIZE
+        self.ui.btn_toggle_menu.clicked.connect(lambda: UIFunctions.toggleMenu(self, 220, True))
+        ## ==> END ##
+
+        ## ==> ADD CUSTOM MENUS
+        self.ui.stackedWidget.setMinimumWidth(20)
+        UIFunctions.addNewMenu(self, "HOME", "btn_home", "url(:/16x16/icons/16x16/cil-home.png)", True)
+        #UIFunctions.addNewMenu(self, "Add User", "btn_new_user", "url(:/16x16/icons/16x16/cil-user-follow.png)", True)
+        UIFunctions.addNewMenu(self, "APPOINTMENTS", "btn_appointments", "url(:/16x16/icons/16x16/cil-calendar-check.png)", True)
+        UIFunctions.addNewMenu(self, "SPECIAL SERVICES", "btn_special_services", "url(:/16x16/icons/16x16/cil-clipboard.png)", True)
+        UIFunctions.addNewMenu(self, "ROOM RESERVATION", "btn_roomNkey",
+                                     "url(:/16x16/icons/16x16/cil-door.png)", True)
+        UIFunctions.addNewMenu(self, "CUSTOM WIDGETS", "btn_widgets", "url(:/16x16/icons/16x16/cil-equalizer.png)", False)
+        ## ==> END ##
+
+        # START MENU => SELECTION
+        UIFunctions.selectStandardMenu(self, "btn_home")
+        ## ==> END ##
+
+        ## ==> START PAGE
+        self.ui.stackedWidget.setCurrentWidget(self.ui.page_home)
+        ## ==> END ##
+
+        ## USER ICON ==> SHOW HIDE
+        UIFunctions.userIcon(self, "WM", "", True)
+        ## ==> END ##
+
+
+        ## ==> MOVE WINDOW / MAXIMIZE / RESTORE
+        ########################################################################
+        def moveWindow(event):
+            # IF MAXIMIZED CHANGE TO NORMAL
+            if UIFunctions.returnStatus() == 1:
+                UIFunctions.maximize_restore(self)
+
+            # MOVE WINDOW
+            if event.buttons() == Qt.LeftButton:
+                self.move(self.pos() + event.globalPos() - self.dragPos)
+                self.dragPos = event.globalPos()
+                event.accept()
+
+        # WIDGET TO MOVE
+        self.ui.frame_label_top_btns.mouseMoveEvent = moveWindow
+        ## ==> END ##
+
+        ## ==> LOAD DEFINITIONS
+        ########################################################################
+        UIFunctions.uiDefinitions(self)
+        ## ==> END ##
+
+        ########################################################################
+        ## END - WINDOW ATTRIBUTES
+        ############################## ---/--/--- ##############################
+
+
+
+
+        ########################################################################
+        #                                                                      #
+        ## START -------------- WIDGETS FUNCTIONS/PARAMETERS ---------------- ##
+        #                                                                      #
+        ## ==> USER CODES BELLOW                                              ##
+        ########################################################################
+
+
+
+        ## ==> QTableWidget RARAMETERS
+        ########################################################################
+        self.ui.tableWidget.horizontalHeader().setSectionResizeMode(QtWidgets.QHeaderView.Stretch)
+        ## ==> END ##
+
+
+
+        ########################################################################
+        #                                                                      #
+        ## END --------------- WIDGETS FUNCTIONS/PARAMETERS ----------------- ##
+        #                                                                      #
+        ############################## ---/--/--- ##############################
+
+
+        ## SHOW ==> MAIN WINDOW
+        ########################################################################
+        self.show()
+        ## ==> END ##
+
+    ## TEMPORARY SOLUTION TO ADMIN NAVIGATION
+    def popMenuInits(self):
+        self.ui.page_appointments_changeUserF = QFrame(self.ui.page_appointments)
+        self.ui.page_appointments_changeUserF.setObjectName(u"page_appointments_changeUserF")
+        self.ui.horizontalLayout_page_appointments_changeUserF = QHBoxLayout(self.ui.page_appointments_changeUserF)
+        self.ui.horizontalLayout_page_appointments_changeUserF.setContentsMargins(0, 0, 0, 0)
+        self.ui.horizontalLayout_page_appointments_changeUserF.setSpacing(0)
+        self.ui.horizontalLayout_page_appointments_changeUserF.setObjectName(
+            u"self.horizontalLayout_page_appointments_changeUserF")
+
+        self.ui.page_appointments_changeUserButton = QPushButton()
+
+        self.ui.page_appointments_changeUserButton.setObjectName(u'page_appointments_changeUserButton')
+        self.ui.page_appointments_changeUserButton.setFixedSize(20, 12)
+        self.ui.horizontalLayout_page_appointments_changeUserF.addWidget(self.ui.page_appointments_changeUserButton)
+
+        self.ui.verticalLayout_page_appointments.addWidget(self.ui.page_appointments_changeUserF, 0,
+                                              QtCore.Qt.AlignRight | QtCore.Qt.AlignBottom)
+        self.ui.verticalLayout_page_appointments.setStretch(20, 1)
+
+        self.ui.popMenu_appointments = QtWidgets.QMenu()
+        self.ui.popMenu_appointments.setObjectName('popMenu_appointments')
+        self.ui.studAction_appointments = QtWidgets.QAction("Student")
+        self.ui.facAction_appointments = QtWidgets.QAction("Faculty")
+        self.ui.popMenu_appointments.addAction(self.ui.studAction_appointments)
+        self.ui.popMenu_appointments.addAction(self.ui.facAction_appointments)
+        self.ui.studAction_appointments.triggered.connect(
+            lambda: self.ui.stackedWidget_appointments.setCurrentWidget(self.ui.page_student_appnt))
+        self.ui.facAction_appointments.triggered.connect(
+            lambda: self.ui.stackedWidget_appointments.setCurrentWidget(self.ui.page_faculty_appnt))
+
+        self.ui.page_appointments_changeUserButton.setMenu(self.ui.popMenu_appointments)
+
+
+
+        self.ui.page_special_services_changeUserF = QFrame(self.ui.page_special_services)
+        self.ui.page_special_services_changeUserF.setObjectName(u"page_special_services_changeUserF")
+        self.ui.horizontalLayout_page_special_services_changeUserF = QHBoxLayout(self.ui.page_special_services_changeUserF)
+        self.ui.horizontalLayout_page_special_services_changeUserF.setContentsMargins(0, 0, 0, 0)
+        self.ui.horizontalLayout_page_special_services_changeUserF.setSpacing(0)
+        self.ui.horizontalLayout_page_special_services_changeUserF.setObjectName(
+            u"horizontalLayout_page_special_services_changeUserF")
+
+        self.ui.page_special_services_changeUserButton = QPushButton()
+        self.ui.page_special_services_changeUserButton.setFixedSize(20, 12)
+        self.ui.page_special_services_changeUserButton.setObjectName(u'page_special_services_changeUserButton')
+
+        self.ui.horizontalLayout_page_special_services_changeUserF.addWidget(self.ui.page_special_services_changeUserButton)
+
+        self.ui.verticalLayout_page_special_services.addWidget(self.ui.page_special_services_changeUserF, 0,
+                                                  QtCore.Qt.AlignRight | QtCore.Qt.AlignBottom)
+        self.ui.verticalLayout_page_special_services.setStretch(20, 1)
+
+        self.ui.popMenu_special_services = QtWidgets.QMenu()
+        self.ui.popMenu_special_services.setObjectName('popMenu_special_services')
+        self.ui.studAction_special_services = QtWidgets.QAction("Student")
+        self.ui.facAction_special_services = QtWidgets.QAction("Faculty")
+        self.ui.popMenu_special_services.addAction(self.ui.studAction_special_services)
+        self.ui.popMenu_special_services.addAction(self.ui.facAction_special_services)
+        
+        self.ui.studAction_special_services.triggered.connect(lambda: self.ui.stackedWidget_special_services.setCurrentWidget(self.ui.page_student_ss))
+        self.ui.facAction_special_services.triggered.connect(lambda: self.ui.stackedWidget_special_services.setCurrentWidget(self.ui.page_faculty_ss))
+
+        self.ui.page_special_services_changeUserButton.setMenu(self.ui.popMenu_special_services)
+
+
+
+
+        self.ui.page_roomNkey_changeUserF = QFrame(self.ui.page_roomNkey)
+        self.ui.page_roomNkey_changeUserF.setObjectName(u"page_roomNkey_changeUserF")
+        self.ui.horizontalLayout_page_roomNkey_changeUserF = QHBoxLayout(self.ui.page_roomNkey_changeUserF)
+        self.ui.horizontalLayout_page_roomNkey_changeUserF.setContentsMargins(0, 0, 0, 0)
+        self.ui.horizontalLayout_page_roomNkey_changeUserF.setSpacing(0)
+        self.ui.horizontalLayout_page_roomNkey_changeUserF.setObjectName(u"horizontalLayout_page_roomNkey_changeUserF")
+
+        self.ui.page_roomNkey_changeUserButton = QPushButton()
+        self.ui.page_roomNkey_changeUserButton.setFixedSize(20, 12)
+        self.ui.page_roomNkey_changeUserButton.setObjectName(u'page_roomNkey_changeUserButton')
+
+        self.ui.horizontalLayout_page_roomNkey_changeUserF.addWidget(self.ui.page_roomNkey_changeUserButton)
+
+        self.ui.verticalLayout_page_roomNkey.addWidget(self.ui.page_roomNkey_changeUserF, 0,
+                                                    QtCore.Qt.AlignRight | QtCore.Qt.AlignBottom)
+        self.ui.verticalLayout_page_roomNkey.setStretch(20, 1)
+
+        self.ui.popMenu_roomNkey = QtWidgets.QMenu()
+        self.ui.popMenu_roomNkey.setObjectName('popMenu_roomNkey')
+        self.ui.studAction_roomNkey = QtWidgets.QAction("Student")
+        self.ui.facAction_roomNkey = QtWidgets.QAction("Faculty")
+        self.ui.admAction_roomNkey = QtWidgets.QAction("Admin")
+        self.ui.popMenu_roomNkey.addAction(self.ui.studAction_roomNkey)
+        self.ui.popMenu_roomNkey.addAction(self.ui.facAction_roomNkey)
+        self.ui.popMenu_roomNkey.addAction(self.ui.admAction_roomNkey)
+        self.ui.studAction_roomNkey.triggered.connect(
+            lambda: self.ui.page_stackedWidget_roomNkey.setCurrentWidget(self.ui.page_student_roomNkey))
+        self.ui.facAction_roomNkey.triggered.connect(
+            lambda: self.ui.page_stackedWidget_roomNkey.setCurrentWidget(self.ui.page_faculty_roomNkey))
+        self.ui.admAction_roomNkey.triggered.connect(
+            lambda: self.ui.page_stackedWidget_roomNkey.setCurrentWidget(self.ui.page_admin_roomNkey))
+
+        self.ui.page_roomNkey_changeUserButton.setMenu(self.ui.popMenu_roomNkey)
+        self.ui.student_pushButton_room1.clicked.connect(self.openwindow)
+        self.ui.student_pushButton_room2.clicked.connect(self.openwindow)
+        self.ui.student_pushButton_room3.clicked.connect(self.openwindow)
+        self.ui.student_pushButton_room4.clicked.connect(self.openwindow)
+        self.ui.student_pushButton_room5.clicked.connect(self.openwindow)
+        self.ui.student_pushButton_room6.clicked.connect(self.openwindow)
+        self.ui.faculty_pushButton_room1.clicked.connect(self.openwindow)
+        self.ui.faculty_pushButton_room2.clicked.connect(self.openwindow)
+        self.ui.faculty_pushButton_room3.clicked.connect(self.openwindow)
+        self.ui.faculty_pushButton_room4.clicked.connect(self.openwindow)
+        self.ui.faculty_pushButton_room5.clicked.connect(self.openwindow)
+        self.ui.faculty_pushButton_room6.clicked.connect(self.openwindow)
+
+
+        self.ui.page_appointments_changeUserButton.setText(QCoreApplication.translate("MainWindow", "Change User UI"))
+        self.ui.page_special_services_changeUserButton.setText(QCoreApplication.translate("MainWindow", "Change User UI"))
+        self.ui.page_roomNkey_changeUserButton.setText(QCoreApplication.translate("MainWindow", "Change User UI"))
+
+
+    
+    ########################################################################
+    ## MENUS ==> DYNAMIC MENUS FUNCTIONS
+    ########################################################################
+    def Button(self):
+        # GET BT CLICKED
+        btnWidget = self.sender()
+
+        # PAGE HOME
+        if btnWidget.objectName() == "btn_home":
+            self.ui.stackedWidget.setCurrentWidget(self.ui.page_home)
+            UIFunctions.resetStyle(self, "btn_home")
+            UIFunctions.labelPage(self, "Home")
+            btnWidget.setStyleSheet(UIFunctions.selectMenu(btnWidget.styleSheet()))
+
+        # PAGE APPOINTMENTS
+        if btnWidget.objectName() == "btn_appointments":
+            self.ui.stackedWidget.setCurrentWidget(self.ui.page_appointments)
+            UIFunctions.resetStyle(self, "btn_appointments")
+            UIFunctions.labelPage(self, "Appointments")
+            btnWidget.setStyleSheet(UIFunctions.selectMenu(btnWidget.styleSheet()))
+
+        # PAGE SPECIAL SERVICES
+        if btnWidget.objectName() == "btn_special_services":
+            self.ui.stackedWidget.setCurrentWidget(self.ui.page_special_services)
+            UIFunctions.resetStyle(self, "btn_special_services")
+            UIFunctions.labelPage(self, "Special Services")
+            btnWidget.setStyleSheet(UIFunctions.selectMenu(btnWidget.styleSheet()))
+
+        if btnWidget.objectName() == "btn_roomNkey":
+            self.ui.stackedWidget.setCurrentWidget(self.ui.page_roomNkey)
+            UIFunctions.resetStyle(self, "btn_roomNkey")
+            UIFunctions.labelPage(self, "Special Services")
+            btnWidget.setStyleSheet(UIFunctions.selectMenu(btnWidget.styleSheet()))
+        """
+        ADDITIONAL PAGES HERE
+        """
+
+        # PAGE WIDGETS
+        if btnWidget.objectName() == "btn_widgets":
+            self.ui.stackedWidget.setCurrentWidget(self.ui.page_widgets)
+            UIFunctions.resetStyle(self, "btn_widgets")
+            UIFunctions.labelPage(self, "Custom Widgets")
+            btnWidget.setStyleSheet(UIFunctions.selectMenu(btnWidget.styleSheet()))
+
+    ## ==> END ##
+
+    ########################################################################
+    ## START ==> APP EVENTS
+    ########################################################################
+
+    ## EVENT ==> MOUSE DOUBLE CLICK
+    ########################################################################
+    def eventFilter(self, watched, event):
+        if watched == self.le and event.type() == QtCore.QEvent.MouseButtonDblClick:
+            print("pos: ", event.pos())
+    ## ==> END ##
+
+    ## EVENT ==> MOUSE CLICK
+    ########################################################################
+    def mousePressEvent(self, event):
+        self.dragPos = event.globalPos()
+        if event.buttons() == Qt.LeftButton:
+            print('Mouse click: LEFT CLICK')
+        if event.buttons() == Qt.RightButton:
+            print('Mouse click: RIGHT CLICK')
+        if event.buttons() == Qt.MidButton:
+            print('Mouse click: MIDDLE BUTTON')
+    ## ==> END ##
+
+    ## EVENT ==> KEY PRESSED
+    ########################################################################
+    def keyPressEvent(self, event):
+        print('Key: ' + str(event.key()) + ' | Text Press: ' + str(event.text()))
+    ## ==> END ##
+
+    ## EVENT ==> RESIZE EVENT
+    ########################################################################
+    def resizeEvent(self, event):
+        self.resizeFunction()
+        return super(MainWindow, self).resizeEvent(event)
+
+    def resizeFunction(self):
+        print('Height: ' + str(self.height()) + ' | Width: ' + str(self.width()))
+    ## ==> END ##
+
+    ########################################################################
+    ## END ==> APP EVENTS
+    ############################## ---/--/--- ##############################
+    ##TEMPORARY SOLUTION FOR POPUP SCHEDULE AND TIME
+    def openwindow(self):
+        QWidget.__init__(self)
+        self.ui = Ui_Form()
+        self.ui.setupUi(self)
+        self.show()
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    QtGui.QFontDatabase.addApplicationFont('fonts/segoeui.ttf')
+    QtGui.QFontDatabase.addApplicationFont('fonts/segoeuib.ttf')
+    window = MainWindow()
+    sys.exit(app.exec_())
